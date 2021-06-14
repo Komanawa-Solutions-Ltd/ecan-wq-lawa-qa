3b655cc3f0eb97fbf7dd3adc4f835e0d167d39c6
mike <mullenkamp1@gmail.com>
2021-06-15 11:43:07 +1200

    update

--- app/main.py
+++ app/main.py
@@ -39,7 +39,7 @@ datasets = param['source']['datasets']
 base_url = param['source']['api_endpoint']
 hts_list = param['source']['hts']
 
-summ_cols = ['site name', 'measurement', 'total count', 'DTL count', 'start date', 'end date', 'min', 'median', 'mean', 'max', 'standard deviation']
+summ_cols = ['Site', 'Measurement', 'total count', 'DTL count', 'start date', 'end date', 'min', 'median', 'mean', 'max', 'standard deviation']
 
 ## Dash parameters
 server = flask.Flask(__name__)
@@ -54,7 +54,7 @@ zoom1 = 7
 
 mapbox_access_token = "pk.eyJ1IjoibXVsbGVua2FtcDEiLCJhIjoiY2pudXE0bXlmMDc3cTNxbnZ0em4xN2M1ZCJ9.sIOtya_qe9RwkYXj5Du1yg"
 
-map_layout = dict(mapbox = dict(layers = [], accesstoken = mapbox_access_token, style = "outdoors", center=dict(lat=lat1, lon=lon1), zoom=zoom1), margin = dict(r=0, l=0, t=0, b=0), autosize=True, hovermode='closest', height=map_height, showlegend = True, legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
+map_layout = dict(mapbox = dict(layers = [], accesstoken = mapbox_access_token, style = "outdoors", center=dict(lat=lat1, lon=lon1), zoom=zoom1), margin = dict(r=0, l=0, t=0, b=0), autosize=True, hovermode='closest', height=map_height, showlegend = False)
 
 ###########################################################
 ### Helper functions
@@ -108,13 +108,18 @@ def get_results(base_url, hts, mtype, ref):
 
     """
     ### Get data
-    res1 = ws.get_data(base_url, hts, ref, mtype).Value
+    res_list = []
+    for s in ref:
+        res = ws.get_data(base_url, hts, s, mtype).Value
+        res_list.append(res)
+
+    res1 = pd.concat(res_list)
 
     ### Process DTLs
     dtl1 = res1[res1.str.contains('<')]
     dtl1 = pd.to_numeric(dtl1.str.replace('<', ''))
     dtl2 = res1[res1.str.contains('>')]
-    dtl2 = pd.to_numeric(dtl2.str.replace('<', ''))
+    dtl2 = pd.to_numeric(dtl2.str.replace('>', ''))
     dtl3 = pd.concat([dtl1, dtl2])
 
     ### Remove DTLs from results
@@ -122,23 +127,32 @@ def get_results(base_url, hts, mtype, ref):
     res2 = pd.to_numeric(res2, errors='coerce').dropna()
 
     ### Run stats
-    dtl_count = len(dtl3)
-    data_count = len(res2)
-    total_count = dtl_count + data_count
-    mean1 = round(res2.mean(), 3)
-    median1 = round(res2.median(), 3)
-    max1 = round(res2.max(), 3)
-    min1 = round(res2.min(), 3)
-    std1 = round(res2.std(), 3)
-    dates1 = res2.reset_index()['DateTime']
-    from_date = dates1.min()
-    to_date = dates1.max()
+    grp1 = res2.reset_index().groupby(['Site', 'Measurement'])
+    dtl_count = dtl3.reset_index().groupby(['Site', 'Measurement']).Value.count()
+    dtl_count.name = 'DTL count'
+    data_count = grp1.Value.count()
+    total_count = data_count.add(dtl_count, fill_value=0).astype(int)
+    total_count.name = 'total count'
+    mean1 = grp1.Value.mean().round(3)
+    mean1.name = 'mean'
+    median1 = grp1.Value.median().round(3)
+    median1.name = 'median'
+    max1 = grp1.Value.max().round(3)
+    max1.name = 'max'
+    min1 = grp1.Value.min().round(3)
+    min1.name = 'min'
+    std1 = grp1.Value.std().round(3)
+    std1.name = 'standard deviation'
+    from_date = grp1['DateTime'].min()
+    from_date.name = 'start date'
+    to_date = grp1['DateTime'].max()
+    to_date.name = 'end date'
 
     ### Make stats df
-    stats_df1 = pd.DataFrame([[ref, mtype, total_count, dtl_count, from_date, to_date, min1, median1, mean1, max1, std1]], columns=summ_cols)
+    stats_df1 = pd.concat([total_count, dtl_count, from_date, to_date, min1, median1, mean1, max1, std1], axis=1)
 
     ### return
-    return res2.reset_index(), dtl3, stats_df1
+    return res2, dtl3, stats_df1
 
 
 # res2, stats_df1 = get_results(base_url, hts, mtype, ref)
@@ -170,19 +184,20 @@ def serve_layout():
         dcc.Dropdown(options=[{'label': d, 'value': d} for d in hts_list], value=None, id='hts', clearable=False),
         html.Label('Measurement'),
         dcc.Dropdown(options=[{'label': d, 'value': d} for d in datasets], value=None, id='parameters'),
-        html.Label('Site name'),
-        dcc.Dropdown(options=[], id='sites', optionHeight=40),
+        html.Label('Site reference'),
+        dcc.Dropdown(options=[], id='sites', multi=True),
         dcc.Link(html.Img(src=app.get_asset_url('ecan.png'), height=100), href='https://www.ecan.govt.nz/')
         ],
     className='two columns', style={'margin': 20}),
 
     html.Div([
-        html.P('Click on a site:', style={'display': 'inline-block'}),
+        html.P('Click on a site or select a group of sites (up to 10):', style={'display': 'inline-block'}),
         dcc.Graph(
             id = 'site-map',
             style={'height': map_height},
             figure=dict(data = [dict(
                                     type = 'scattermapbox',
+                                    name='site',
                                     hoverinfo = 'text',
                                     marker = dict(
                                             size=8,
@@ -212,6 +227,17 @@ def serve_layout():
     dash_table.DataTable(
         id='summ_table',
         columns=[{"name": v, "id": v, 'deletable': True} for v in summ_cols],
+        data=[],
+        sort_action="native",
+        sort_mode="multi",
+        style_cell={
+            'minWidth': '80px', 'maxWidth': '200px',
+            'whiteSpace': 'normal'
+        }
+        ),
+    dash_table.DataTable(
+        id='dtl_table',
+        columns=[{"name": v, "id": v, 'deletable': True} for v in ['Site', 'Measurement', 'DTL value']],
         data=[],
         sort_action="native",
         sort_mode="multi",
@@ -269,7 +295,8 @@ def update_site_data(hts, parameter):
     [Output('ts_data', 'data'), Output('summ_data', 'data'), Output('dtl_data', 'data')],
     [Input('hts', 'value'), Input('parameters', 'value'), Input('sites', 'value')])
 def update_ts_data(hts, parameter, site):
-    if (hts is None) or (parameter is None) or (site is None):
+    # print(site)
+    if (hts is None) or (parameter is None) or (len(site) == 0):
         print('No data available')
         return None, None, None
     else:
@@ -297,17 +324,28 @@ def update_display_map(site_data_str, figure):
         lat1 =  sites1.lat.tolist()
         names1 = sites1.ref.tolist()
 
-        data = [dict(
-            lat = lat1,
-            lon = lon1,
-            text = names1,
-            ids=names1,
-            type = 'scattermapbox',
-            hoverinfo = 'text',
-            marker = dict(size=8, color='black', opacity=1)
-        )]
+        fig = go.Figure(go.Scattermapbox(lat = lat1,
+                                lon = lon1,
+                                text = names1,
+                                ids=names1,
+                                hoverinfo = 'text',
+                                marker = dict(size=8, color='black', opacity=1)
+                                )
+                        )
 
-        fig = dict(data=data, layout=figure['layout'])
+        fig.update_layout(**figure['layout'])
+
+        # data = [dict(
+        #     lat = lat1,
+        #     lon = lon1,
+        #     text = names1,
+        #     ids=names1,
+        #     type = 'scattermapbox',
+        #     hoverinfo = 'text',
+        #     marker = dict(size=8, color='black', opacity=1)
+        # )]
+
+        # fig = dict(data=data, layout=figure['layout'])
 
     return fig
 
@@ -319,22 +357,18 @@ def update_display_map(site_data_str, figure):
 def update_sites_values(selectedData, clickData):
     # print(clickData)
     # print(selectedData)
+    site1_id = []
+
     if selectedData:
-        sel1 = selectedData['points'][0]
-        if 'id' in sel1:
-            site1_id = sel1['id']
-        else:
-            site1_id = None
+        sel1 = [s['id'] for s in selectedData['points'] if 'id' in s]
+        if sel1:
+            site1_id = sel1[:10]
     elif clickData:
         sel1 = clickData['points'][0]
         if 'id' in sel1:
-            site1_id = sel1['id']
-        else:
-            site1_id = None
-    else:
-        site1_id = None
+            site1_id = [sel1['id']]
 
-    # print(sites1_id)
+    # print(site1_id)
 
     return site1_id
 
@@ -347,24 +381,137 @@ def update_sites_values(selectedData, clickData):
 def update_summ_table(summ_data_str):
     if summ_data_str is not None:
         summ_data = decode_df(summ_data_str)
-        summ_table = summ_data.to_dict('records')
+        summ_table = summ_data.reset_index().to_dict('records')
 
         return summ_table
     else:
         return []
 
 
+@app.callback(
+    Output('dtl_table', 'data'),
+    [Input('dtl_data', 'data')],
+    # [State('stn_dict', 'data'), State('method_dd', 'value'), State('active_select', 'value')]
+    )
+def update_dtl_table(dtl_data_str):
+    if dtl_data_str is not None:
+        dtl_data = decode_df(dtl_data_str).reset_index().drop('DateTime', axis=1).drop_duplicates()
+        dtl_data.rename(columns={'Value': 'DTL value'}, inplace=True)
+        dtl_table = dtl_data.to_dict('records')
+
+        return dtl_table
+    else:
+        return []
+
+
 @app.callback(Output('plots', 'children'),
               [Input('plot_tabs', 'value'), Input('ts_data', 'data')],
-              # [State('stn_dict', 'data'), State('method_dd', 'value'), State('active_select', 'value'), State('allo_ds_id', 'data'), State('flow_use_ds_id', 'data'), State('last_month', 'data'), State('last_year', 'data')]
+              [State('dtl_data', 'data')]
               )
-def render_plot(tab, ts_data_str):
+def render_plot(tab, ts_data_str, dtl_data_str):
 
     # print(flow_stn_id)
 
     info_str = """
             ### Intro
-            This is the [Environment Southland](https://www.es.govt.nz/) streamflow naturalisation, surface water usage, and surface water allocation dashboard.
+            This is the [Environment Canterbury](https://www.ecan.govt.nz/) surface water quality dashboard for checking data quality.
+
+            ### Brief Guide
+        """
+
+    print(tab)
+
+    if tab == 'info_tab':
+        fig1 = info_str
+
+    else:
+        if ts_data_str is not None:
+            ts_data = decode_df(ts_data_str).reset_index()
+            dtl_data = decode_df(dtl_data_str).reset_index().drop('DateTime', axis=1).drop_duplicates()
+
+            if tab == 'box_plot':
+                fig1 = go.Figure(data=go.Box(y=ts_data['Value'],
+                                             # name=ts_data['Site'],
+                                boxmean=True,
+                                opacity=1.0,
+                                x=ts_data['Site'],
+                                boxpoints='all',
+                                showlegend=False,
+                                ),
+                                 layout = dict(
+                                                # title=stn_ref,
+                                                # yaxis={'title': 'Flow rate (m3/s)'},
+                                                # xaxis={'title': 'Date'},
+                                                # dragmode='pan',
+                                                font=dict(size=18),
+                                                hovermode='x',
+                                                paper_bgcolor = '#F4F4F8',
+                                                plot_bgcolor = '#F4F4F8',
+                                                height = ts_plot_height
+                                                )
+                                )
+                fig1.add_trace(
+                        go.Scatter(
+                            mode='markers',
+                            x=dtl_data['Site'],
+                            y=dtl_data['Value'],
+                            name='Detection limits',
+                            marker_symbol='diamond',
+                            marker=dict(
+                                color='rgba(250, 50, 50, 0.5)',
+                                size=12,
+                                line=dict(
+                                    color='DarkSlateGrey',
+                                    width=2
+                                )
+                            )
+                            )
+                        )
+                fig1.update_layout(legend=dict(
+                                    yanchor="top",
+                                    y=-0.06,
+                                    xanchor="right",
+                                    x=0.99
+                                ))
+                fig1.update_yaxes(automargin=True)
+        else:
+            fig1 = info_str
+
+    if isinstance(fig1, str):
+        return dcc.Markdown(fig1)
+    else:
+        fig = dcc.Graph(
+                # id = 'plots',
+                figure = fig1,
+                config={"displaylogo": False, 'scrollZoom': True, 'showLink': False}
+                )
+
+        return fig
+
+
+
+# if __name__ == '__main__':
+#     server.run(host='0.0.0.0', port=80)
+
+if __name__ == '__main__':
+    app.run_server(debug=True, host='0.0.0.0', port=8080)
+
+# if __name__ == '__main__':
+#     app.run_server(host='0.0.0.0', port=8080)
+
+
+
+
+
+
+
+
+
+
+
+info_str = """
+            ### Intro
+            This is the [Environment Canterbury](https://www.ecan.govt.nz/) surface water quality dashboard for checking data quality.
 
             ### Brief Guide
             #### Selecting datasets
@@ -394,71 +541,6 @@ def render_plot(tab, ts_data_str):
             ### More info
             A more thorough description of the streamflow naturalisation method can be found [here](https://github.com/mullenkamp/nz-flow-naturalisation/blob/main/README.rst).
         """
-    print(tab)
-
-    if tab == 'info_tab':
-        fig1 = info_str
-
-    else:
-        if ts_data_str is not None:
-            ts_data = decode_df(ts_data_str)
-
-            if tab == 'box_plot':
-                fig1 = go.Figure(data=go.Box(y=ts_data['Value'],
-                                boxmean=True,
-                                name=ts_data.Site.iloc[0],
-                                opacity=1.0,
-                                x=ts_data['Site'],
-                                boxpoints='all',
-                                ),
-                                 layout = dict(
-                                                # title=stn_ref,
-                                                # yaxis={'title': 'Flow rate (m3/s)'},
-                                                # xaxis={'title': 'Date'},
-                                                # dragmode='pan',
-                                                font=dict(size=18),
-                                                hovermode='x',
-                                                paper_bgcolor = '#F4F4F8',
-                                                plot_bgcolor = '#F4F4F8',
-                                                height = ts_plot_height
-                                                )
-                                                                         )
-        else:
-            fig1 = info_str
-
-    if isinstance(fig1, str):
-        return dcc.Markdown(fig1)
-    else:
-        fig = dcc.Graph(
-                # id = 'plots',
-                figure = fig1,
-                config={"displaylogo": False, 'scrollZoom': True, 'showLink': False}
-                )
-
-        return fig
-
-
-
-# if __name__ == '__main__':
-#     server.run(host='0.0.0.0', port=80)
-
-if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8080)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
